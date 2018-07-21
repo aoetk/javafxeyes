8692e4af441f4406d26e25443ca5f7779122f824
AOE Takashi <aoe1219@gmail.com>
2018-07-21 21:54:49 +0900

    Build layout.

--- src/aoetk/tools/javafxeyes/Main.java
+++ src/aoetk/tools/javafxeyes/Main.java
@@ -1,7 +1,9 @@
 package aoetk.tools.javafxeyes;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -9,8 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        final HBox base = FXMLLoader.load(getClass().getResource("JavaFXEyes.fxml"));
         primaryStage.setTitle("JavaFXEyes");
-        primaryStage.setScene(new Scene(new Eye(), Color.TRANSPARENT));
+        primaryStage.setScene(new Scene(base, Color.TRANSPARENT));
         primaryStage.show();
     }
 
