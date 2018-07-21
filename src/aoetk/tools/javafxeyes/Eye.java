d3ba810cee73aa8f8669dc24540ebafa72cf4397
AOE Takashi <aoe1219@gmail.com>
2018-07-21 22:18:03 +0900

    Add a method updates a position of an eye.

--- src/aoetk/tools/javafxeyes/Eye.java
+++ src/aoetk/tools/javafxeyes/Eye.java
@@ -26,6 +26,35 @@ public class Eye extends StackPane {
         initBind();
     }
 
+    /**
+     * Update a position of an eye.
+     *
+     * @param mouseX Relative x coordinate from this component
+     * @param mouseY Relative y coordinate from this component
+     */
+    public void updateEyePosition(double mouseX, double mouseY) {
+        double localMouseX = mouseX - (this.getWidth() / 2.0);
+        double localMouseY = mouseY - (this.getHeight() / 2.0);
+        computeEyePosition(localMouseX, localMouseY);
+    }
+
+    private void computeEyePosition(double localMouseX, double localMouseY) {
+        double parameter = Math.atan2(localMouseY, localMouseX);
+        final double width = this.getWidth();
+        final double height = this.getHeight();
+
+        double eyeX = width / 4.0 * Math.cos(parameter);
+        if (Math.abs(localMouseX) < Math.abs(eyeX)) {
+            eyeX = localMouseX;
+        }
+        double eyeY = height / 4.0 * Math.cos(parameter);
+        if ((Math.abs(localMouseY) < Math.abs(eyeY))) {
+            eyeY = localMouseY;
+        }
+        eye.setTranslateX(eyeX);
+        eye.setTranslateY(eyeY);
+    }
+
     private void initBind() {
         outerEyeball.radiusXProperty().bind(this.widthProperty().divide(2));
         outerEyeball.radiusYProperty().bind(this.heightProperty().divide(2));
